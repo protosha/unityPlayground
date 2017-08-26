8bae643c467326e4b27e001eae1f8509f06e72bb
Maxim Tsoy <protosha123@gmail.com>
2017-08-26 20:02:53 +0300

    replaced spine library with fixed one

---
+++ Assets/Scripts/Animation/AnimationState.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine.Unity;
+
+public class AnimationState : StateMachineBehaviour {
+    [SerializeField] private string animationName;
+    [SerializeField] private bool loop;
+    [SerializeField] private float speed = 1;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        SkeletonAnimation anim = animator.GetComponent<SkeletonAnimation>();
+        anim.state.SetAnimation(0, this.animationName, this.loop).timeScale = speed;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public enum Direction { Right, Left }
+
+public class DirectionEventArgs : EventArgs {
+    private Direction dir;
+
+    public DirectionEventArgs(Direction dir) {
+        this.dir = dir;
+    }
+
+    public Direction Direction {
+        get { return this.dir; }
+    }
+}
+
+public class PlayerController : MonoBehaviour {
+    [SerializeField] private float walkSpeed = 0;
+    [SerializeField] private float runSpeed = 0;
+
+    private Direction direction;
+
+    Rigidbody2D playerRB;
+    Animator playerAnimator;
+
+    delegate void DirectionChangedHandler(DirectionEventArgs e);
+    event DirectionChangedHandler DirectionChanged;
+
+    public Direction FaceDirection {
+        get {
+            return this.direction;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        playerRB = this.GetComponent<Rigidbody2D>();
+        playerAnimator = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    void FixedUpdate() {
+        float moveAxis = Input.GetAxis("Horizontal");
+    }
+}
